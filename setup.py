863f05e12649a57c33bc479cddfac8159e7373c3
Moniica990 <moniicar990@gmail.com>
2025-05-26 22:20:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='Information retrieval',
+    version='0.0.0',
+    author='Moniica',
+    author_email='moniicar990!@gmail.com',
+    packages=find_packages(),
+    #find_packages() --this will help to insatll src as local package
+    install_requires=[]
+
+
+)
